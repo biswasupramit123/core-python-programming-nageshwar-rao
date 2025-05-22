a7228832a461cecd91973ce5da4c719f05d44dcc
biswasupramit123 <biswasupramit123@gmail.com>
2025-05-22 12:55:19 +0530

    initial commit: convert octal, binary and hexadecimal numbers into decimal base

---
+++ Chapter03_Datatypes/conv2dec.py
@@ -0,0 +1,12 @@
+# python program to convert octal, binary and hexadecimal numbers into decimal
+c1 = 0o10
+c2 = 0B1110010
+c3 = 0x1c2
+
+n = int(c1)
+print("Octal to Decimal: ", n)
+n = int(c2)
+print("Binary to Decimal: ", n)
+n = int(c3)
+print("Hexadecimal to Decimal: ", n)
+# int() function converts the given number into integer
